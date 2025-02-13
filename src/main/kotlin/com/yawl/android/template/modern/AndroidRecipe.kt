297a0b9ad24536ef15ecfeab79452d0b209d3dff
Bob Nicholson <bobniklv@gmail.com>
2025-02-14 00:05:36 +0300

    :fire: Remove unused addAllKotlinDependencies

--- src/main/kotlin/com/yawl/android/template/modern/AndroidRecipe.kt
+++ src/main/kotlin/com/yawl/android/template/modern/AndroidRecipe.kt
@@ -1,7 +1,6 @@
 package com.yawl.android.template.modern
 
 import com.android.tools.idea.wizard.template.RecipeExecutor
-import com.android.tools.idea.wizard.template.impl.activities.common.addAllKotlinDependencies
 import com.yawl.android.template.modern.activity.main
 import com.yawl.android.template.modern.app.appBuild
 import com.yawl.android.template.modern.buildLogic.buildLogicWriter
@@ -11,7 +10,6 @@ fun RecipeExecutor.modernAndroidRecipe(
     parameters: MaTemplateParameters
 ) {
     val moduleData = parameters.moduleData
-    addAllKotlinDependencies(data = moduleData)
     buildLogicWriter(moduleData = moduleData)
     libsTomlFile(moduleData = moduleData)
     appBuild(moduleData = moduleData)
