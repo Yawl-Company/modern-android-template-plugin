8f808a50bfe083a0afde0fe08376dc26ea504f13
Bob Nicholson <bobniklv@gmail.com>
2025-02-13 17:35:07 +0300

    :sparkles: Add libs.versions.toml file to gradle folder

--- src/main/kotlin/com/yawl/matemplate/AndroidRecipe.kt
+++ src/main/kotlin/com/yawl/matemplate/AndroidRecipe.kt
@@ -4,10 +4,12 @@ import com.android.tools.idea.wizard.template.ModuleTemplateData
 import com.android.tools.idea.wizard.template.RecipeExecutor
 import com.android.tools.idea.wizard.template.impl.activities.common.addAllKotlinDependencies
 import com.yawl.matemplate.buildLogic.buildLogicWriter
+import com.yawl.matemplate.libs.libsTomlFile
 
 fun RecipeExecutor.modernAndroidRecipe(
     moduleData: ModuleTemplateData
 ) {
     addAllKotlinDependencies(data = moduleData)
     buildLogicWriter(moduleData = moduleData)
+    libsTomlFile(moduleData = moduleData)
 }
