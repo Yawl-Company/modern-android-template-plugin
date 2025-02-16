c5737c9fd61d13e3cf5bff13378a96a0b6376d45
Bob Nicholson <bobniklv@gmail.com>
2025-02-16 18:29:33 +0300

    :art: Refactor...

--- src/main/kotlin/com/yawl/android/template/core/extension/LibsExtensions.kt
+++ src/main/kotlin/com/yawl/android/template/core/extension/LibsExtensions.kt
@@ -4,14 +4,17 @@ import com.yawl.android.template.core.libs.LibraryBuildGradle
 import com.yawl.android.template.core.libs.PluginBuildGradle
 
 fun List<PluginBuildGradle>.pluginsSection(apply: Boolean): String {
-    val spaces = "\n    "
-    val list = this.joinToString(spaces) { plugin -> plugin.apply(apply) }
-    return "plugins {$spaces$list\n}"
+    val list = this.map { plugin -> plugin.apply(apply) }
+    return list.section("plugins")
 }
 
 fun List<LibraryBuildGradle>.librariesDependencies(): String {
-    val spaces = "\n    "
-    val list = this.joinToString(spaces) { library -> library.implementation() }
-    return "dependencies {$spaces$list\n}"
+    val list = this.map { library -> library.implementation() }
+    return list.section("dependencies")
 }
 
+fun List<String>.section(name: String): String {
+    val spaces = "\n    "
+    val list = this.joinToString(spaces)
+    return "$name {$spaces$list\n}"
+}
