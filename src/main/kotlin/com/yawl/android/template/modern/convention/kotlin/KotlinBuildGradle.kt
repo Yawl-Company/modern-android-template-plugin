886629324363dae18422f12ecd45d6f15f979e0a
Bob Nicholson <bobniklv@gmail.com>
2025-02-16 19:58:15 +0300

    :art: Refactor...

--- src/main/kotlin/com/yawl/android/template/modern/convention/kotlin/KotlinBuildGradle.kt
+++ src/main/kotlin/com/yawl/android/template/modern/convention/kotlin/KotlinBuildGradle.kt
@@ -1,7 +1,15 @@
 package com.yawl.android.template.modern.convention.kotlin
 
+import com.yawl.android.template.core.convention.conventionPluginsRegistration
+import com.yawl.android.template.core.libs.usage.conventionKotlinJvm
+import com.yawl.android.template.core.libs.usage.conventionKotlinJvmAndroid
+import com.yawl.android.template.core.libs.usage.conventionKotlinSerialization
+import com.yawl.android.template.core.libs.usage.conventionKsp
+
 internal fun kotlinBuildGradle(): String {
-    return """
+    return buildString {
+        appendLine(
+        """
         plugins {
             kotlin("jvm")
             `java-gradle-plugin`
@@ -14,29 +22,20 @@ internal fun kotlinBuildGradle(): String {
             implementation(libs.kotlin.gradlePlugin)
             implementation(libs.ksp.gradlePlugin)
         }
+        """
+        )
 
-        gradlePlugin {
-            plugins {
-                create("kotlin-jvm") {
-                    id = "convention.kotlin-jvm"
-                    implementationClass = "com.convention.jvm.KotlinJvmPlugin"
-                }
+        appendLine()
 
-                create("kotlin-jvm-android") {
-                    id = "convention.kotlin-jvm-android"
-                    implementationClass = "com.convention.jvm.KotlinJvmAndroidPlugin"
-                }
-
-                create("ksp") {
-                    id = "convention.ksp"
-                    implementationClass = "com.convention.KspConventionPlugin"
-                }
-
-                create("serialization") {
-                    id = "convention.serialization"
-                    implementationClass = "com.convention.KotlinSerializationConventionPlugin"
-                }
-            }
-        }
-    """.trimIndent()
+        appendLine(
+            conventionPluginsRegistration(
+                listOfNotNull(
+                    conventionKotlinJvm to "com.convention.jvm.KotlinJvmPlugin",
+                    conventionKotlinJvmAndroid to "com.convention.jvm.KotlinJvmAndroidPlugin",
+                    conventionKsp to "com.convention.KspConventionPlugin",
+                    conventionKotlinSerialization to "com.convention.KotlinSerializationPlugin",
+                )
+            )
+        )
+    }
 }
