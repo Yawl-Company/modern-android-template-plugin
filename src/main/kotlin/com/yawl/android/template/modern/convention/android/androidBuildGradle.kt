fa647255e5b707865c493ef797c1f9b56f4162c0
Bob Nicholson <bobniklv@gmail.com>
2025-02-16 15:11:12 +0300

    :truck: Refactor...

--- src/main/kotlin/com/yawl/android/template/modern/convention/android/androidBuildGradle.kt
+++ src/main/kotlin/com/yawl/android/template/modern/convention/android/androidBuildGradle.kt
@@ -21,7 +21,6 @@ internal fun androidBuildGradle(template: IModernTemplate): String {
                     implementation(libs.room.gradlePlugin)
                 """ else ""
     }
-            implementation(libs.room.gradlePlugin)
             // workaround for https://github.com/gradle/gradle/issues/15383
             implementation(files((libs as Any).javaClass.superclass.protectionDomain.codeSource.location))
         }
