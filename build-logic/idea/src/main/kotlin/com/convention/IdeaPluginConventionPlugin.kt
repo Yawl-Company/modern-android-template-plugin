505123bdf3f26c99970c235d041df23c42883882
Bob Nicholson <bobniklv@gmail.com>
2025-02-18 21:54:35 +0300

    :sparkles: Update

--- build-logic/idea/src/main/kotlin/com/convention/IdeaPluginConventionPlugin.kt
+++ build-logic/idea/src/main/kotlin/com/convention/IdeaPluginConventionPlugin.kt
@@ -1,12 +1,13 @@
 package com.convention
 
+import com.convention.jvm.KotlinJvmPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class IdeaPluginConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            plugins.apply(libs.plugins.convention.kotlin.jvm.get().pluginId)
+            plugins.apply(KotlinJvmPlugin::class.java)
             plugins.apply(libs.plugins.jetbrains.intellij.get().pluginId)
         }
     }
