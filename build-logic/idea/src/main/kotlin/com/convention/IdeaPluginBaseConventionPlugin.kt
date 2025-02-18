9cff646af83a07f92db70c0a650fca6b0ac6338b
Bob Nicholson <bobniklv@gmail.com>
2025-02-18 23:28:50 +0300

    :sparkles: Migrate to intellij platform gradle plugin

--- build-logic/idea/src/main/kotlin/com/convention/IdeaPluginBaseConventionPlugin.kt
+++ build-logic/idea/src/main/kotlin/com/convention/IdeaPluginBaseConventionPlugin.kt
@@ -1,44 +1,10 @@
 package com.convention
 
-import com.convention.jvm.KotlinJvmPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.intellij.tasks.PatchPluginXmlTask
-import org.jetbrains.intellij.tasks.PublishPluginTask
-import org.jetbrains.intellij.tasks.SignPluginTask
 
-class IdeaPluginBaseConventionPlugin: Plugin<Project> {
+class IdeaPluginBaseConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        with(target) {
-            plugins.apply(KotlinJvmPlugin::class.java)
-            plugins.apply(libs.plugins.jetbrains.intellij.get().pluginId)
-
-            tasks.withType(PatchPluginXmlTask::class.java).configureEach {
-                it.apply {
-                    sinceBuild.set(libs.versions.intelliJSinceBuild)
-                    untilBuild.set(
-                        libs.versions.intelliJUntilBuild
-                            .get()
-                            .takeIf { version ->
-                                version != "0"
-                            } ?: ""
-                    )
-                }
-            }
-
-            tasks.withType(SignPluginTask::class.java).configureEach {
-                it.apply {
-                    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-                    privateKey.set(System.getenv("PRIVATE_KEY"))
-                    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-                }
-            }
-
-            tasks.withType(PublishPluginTask::class.java).configureEach {
-                it.apply {
-                    token.set(System.getenv("PUBLISH_TOKEN"))
-                }
-            }
-        }
+        // todo: migrate to gradle 2.0
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.convention.libs
+
 plugins {
-    alias(libs.plugins.convention.intellij.android)
+    alias(libs.plugins.convention.kotlin.jvm)
+    id("org.jetbrains.intellij.platform") version "2.2.1"
 }
 
 group = "com.yawl"
@@ -7,4 +10,38 @@ version = "1.0.1"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        androidStudio("2024.1.2.13")
+        bundledPlugin("org.jetbrains.android")
+    }
+}
+
+
+intellijPlatform {
+    pluginConfiguration {
+        version = libs.versions.intelliJ.get()
+
+        ideaVersion {
+            sinceBuild = libs.versions.intelliJSinceBuild
+            untilBuild = provider { null }
+        }
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
+
+    buildSearchableOptions = false
 }
