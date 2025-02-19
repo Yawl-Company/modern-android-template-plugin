14beabfe2634daf8b89ef4938475f8fead065867
Bob Nicholson <bobniklv@gmail.com>
2025-02-19 11:23:29 +0300

    :lipstick: Refactor...

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,24 +1,14 @@
 @file:Suppress("UnstableApiUsage")
 
-import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
-
-
 rootProject.name = "build-logic"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-plugins {
-    id("org.jetbrains.intellij.platform.settings") version "2.2.1"
-}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        intellijPlatform {
-            defaultRepositories()
-        }
     }
     versionCatalogs {
         create("libs") {
